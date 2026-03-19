1041d1b0aed0dc705467895d2f291d878a03405e
Aiman Faiz <aiman.faiz.dev@protonmail.com>
2026-03-20 03:15:01 +0800

    add basic tests for scripts

---
+++ tests/test_archive.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+test -f ./archive_logs.sh && echo \"[+] archive_logs.sh exists\" || echo \"[!] missing archive_logs.sh\"
+
---
+++ tests/test_backup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+test -f ./backup.sh && echo \"[+] backup.sh exists\" || echo \"[!] missing backup.sh\"
+
